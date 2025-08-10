57d3661512b5ff29f565869e6e9f0d81ef246f45
manasa badeti <manasab340@gmail.com>
2025-08-11 00:48:11 +0530

    deploy code

--- server/db.js
+++ server/db.js
@@ -2,12 +2,19 @@ const Pool = require("pg").Pool;
 const dotenv = require("dotenv");
 //Load env variables
 dotenv.config({ path: "./config/config.env" });
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
